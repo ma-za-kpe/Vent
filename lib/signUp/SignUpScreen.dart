1af7a7dcbe713bbfee097a4bd90b6a8a5ae9f302
partyParty01 <makpalyy@gmail.com>
2020-08-06 11:43:06 -0700

    add signup

---
+++ lib/signUp/SignUpScreen.dart
@@ -0,0 +1,45 @@
+import 'package:flutter/material.dart';
+import 'package:font_awesome_flutter/font_awesome_flutter.dart';
+
+class SignUpScreen extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: Column(
+        mainAxisAlignment: MainAxisAlignment.spaceAround,
+        children: <Widget>[
+          Text(
+            "SignUp",
+            style: TextStyle(fontSize: 40, fontWeight: FontWeight.w600),
+          ),
+          GestureDetector(
+            onTap: () => print("Signed up with Google"),
+            child: Icon(
+              FontAwesomeIcons.googlePlus,
+              color: Color(0xffDC4E41),
+              size: 100,
+            ),
+          ),
+          Row(
+            mainAxisAlignment: MainAxisAlignment.center,
+            children: <Widget>[
+              Text('Already have an account'),
+              SizedBox(
+                width: 20,
+              ),
+              GestureDetector(
+                onTap: () => print("Signed up"),
+                child: Text(
+                g  ' Lo Up ',
+                  style: TextStyle(
+                    color: Color(0xff930000),
+                  ),
+                ),
+              )
+            ],
+          )
+        ],
+      ),
+    );
+  }
+}
